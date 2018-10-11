b0e72bcd6b9afff262aa2b7046cf1d7501fa73d1
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-11 01:03:43 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,8 +25,24 @@ class  MusicLibraryController
     while demand !="exit"
     
     
+    case demand
+    when "list songs"
+    self.list_songs
+    when 'list artists'
+    self.list_artists
+    when 'list genres'
+    self.list_genres
+    when 'list artist'
+    self.list_songs_by_artist
+    when 'list genre'
+    self.list_songs_by_genre
+    when 'play song'
+    self.play_song
+    end
+    
     demand = gets.strip
-  end
+    
+    end
     
    
   end 
@@ -61,8 +77,39 @@ class  MusicLibraryController
       
     end
   
+  end
+  
+    def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre_name = gets.strip
+    
+    
+    genre = Genre.all.find{|a| a if a.name == genre_name}
+    
+    if genre
+      genre_songs = genre.songs 
+      genre_songs.sort_by{|s| s.name }.each_with_index{ |val,index| puts "#{index+1}. #{val.artist.name} - #{val.name}" }
+      
+    end
   
   end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    
+    song_number = gets.strip.to_i
+    
+    songs_sorted = Song.all.sort_by {|song| song.name}
+    if song_number >0 && song_number < songs_sorted.length+1
+    song_name = songs_sorted[song_number-1].name
+    artist_name = songs_sorted[song_number-1].artist.name
+    puts "Playing #{song_name} by #{artist_name}"
+  end
+    
+  end
+  
+  
+  
   
   
 end
